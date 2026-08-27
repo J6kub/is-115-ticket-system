066fc54166eec79a380c3cfbf6a963c8501d389a
Iselin <iselin.rolness03@gmail.com>
2026-08-27 13:30:53 +0200

    log in page

---
+++ app/login-page/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Testink</h1>
+    </header>
+    <div class="login-container">
+        <h1>Logg inn</h1>
+        <form>
+            <label for="username">Brukernavn:</label>
+            <input type="text" id="username" name="username" required><br><br>
+            <label for="password">Passord:</label>  
+            <input type="password" id="password" name="password" required><br><br>
+            <button type="submit">Logg inn</button>
+        </form>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <html>
 	<head>
 		<h1>Testink</h1>
+		<a href="app/login-page/index.php">Login</a>
+		
 	</head>
 </html>
